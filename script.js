2bbcfe65551d08f6ec988321d7bef0323535e825
Elena Morozova <therion231@yandex.ru>
2015-03-12 00:10:45 +0300

    Changing gui: buttons delete/change for every message

--- script.js
+++ script.js
@@ -10,29 +10,34 @@ function run(){
     var loginContainer = document.getElementsByClassName('BlockLogin')[0];
 	loginContainer.addEventListener('click', delegateEvent);
 	var messageContainer = document.getElementsByClassName('BlockChatting')[0];
-	messageContainer.addEventListener('change', delegateEvent);
-	var container = document.getElementsByClassName('BlockMessageWork')[0];
-	container.addEventListener('click', delegateEvent);
+	messageContainer.addEventListener('click', delegateEvent);
 	var index = document.getElementById('Indicator');
 	index.addEventListener('mouseover', delegateEvent);
 	index.addEventListener('mouseout', delegateEvent);
 	
 }
 function delegateEvent(evtObj) {
-	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonSend'))
+	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonSend') && document.getElementsByClassName('Select').length == 0)
 		addMessage();
 	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonLogin'))
 		addUser();
 	if (evtObj.type === 'click' && evtObj.target.classList.contains('ChangeLogin'))
 		changeUserName();
-	if (evtObj.type === 'change' && evtObj.target.nodeName == 'INPUT'){
+	if (evtObj.type === 'click' && evtObj.target.classList.contains('Delete'))
+	{
 		var label = evtObj.target.parentElement;
-	setMarker(label);	
-	}
-	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonDelete'))
+	    setMarker(label);	
 		deleteMessage();
-	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonChange'))
+	}
+	if (evtObj.type === 'click' && evtObj.target.classList.contains('Change'))
+	{
+		var label = evtObj.target.parentElement;
+	    setMarker(label);	
 		editMessage();
+	}
+	var container = document.getElementsByClassName('Select');
+	if (evtObj.type === 'click' && evtObj.target.classList.contains('ButtonSend') && container.length>0)
+		nextEditMessage();
 	if (evtObj.type === 'mouseover')
 		changeIndicatorOn();
 	if (evtObj.type === 'mouseout')
@@ -41,11 +46,13 @@ function delegateEvent(evtObj) {
 
 function createMessage(text){
 	var divItem = document.createElement('div');
-	var button = document.createElement('input');
-	    
+	var button1 = document.createElement('button');
+	var button2 = document.createElement('button');
+	    button1.className = "Delete";
+		button2.className = "Change";
 		divItem.className = 'Message';
-		button.setAttribute("type", "checkbox");
-		divItem.appendChild(button);
+		divItem.appendChild(button1);
+		divItem.appendChild(button2);
 	    divItem.appendChild(document.createTextNode(text));
  
 	return divItem;
@@ -107,27 +114,25 @@ function setMarker(label)
 function deleteMessage()
 {
 	var messages = document.getElementsByClassName('Select');
-	if(messages.length != 0)
-	{
-		for(var i = messages.length - 1; i >= 0; i--)
-			messages[i].remove();
-	}
+	messages[0].remove();
 }
 
 function editMessage()
 {
 	var messages = document.getElementsByClassName('Select');
-	if(messages.length == 1)
 	var str = messages[0].innerHTML;
-    var pos = str.indexOf(">")+1;
-    var text = str.substring(pos);
-	var prom = prompt('Редактирование:', text);
-	if(prom.length != 0)
-	{
-	if (prom != null)
-	messages[0].innerHTML = "<input type=checkbox>"+prom;
-    else messages[0].innerHTML = "<input type=checkbox>"+str;
-	}
+	var pos = str. lastIndexOf(">")+1;
+	var text = str.substring(pos);
+    document.getElementById('ChatText').value = text;
+   
+}
+
+function nextEditMessage()
+{
+	var messages = document.getElementsByClassName('Select');
+	if (getMessage().length!=0)
+	messages[0].innerHTML = '<button class="Delete"></button><button class="Change"></button>'+getMessage();
+	document.getElementById('ChatText').value='';
 	messages[0].classList.remove('Select');
 }
 
@@ -142,3 +147,4 @@ function changeIndicatorOff()
 	var index = document.getElementById('Indicator');
 	index.setAttribute('src','http://savepic.su/5187513.png');
 }
+
